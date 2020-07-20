5fdae9064d04e4810f551a03b9a26b0cfd28b922
Shannon Maloy <ShannonMaloy@shannons-mbp.attlocal.net>
2020-07-20 17:19:37 -0400

    Created the dice, added functionality to roll dice by roll button

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 console.log("JS IS CONNECTED")
+const diceOptions = [1, 2, 3, 4, 5, 6]
+
+
+
+//click to roll rice
+const roll = document.querySelector('.roll')
+roll.addEventListener('click',rollDice)
+
+function rollDice(){
+const dice = document.querySelectorAll('.dice')
+console.log(dice)
+dice.forEach((die) => {
+        console.log(dice)
+        let diceRoll = Math.ceil(Math.random() * diceOptions.length)
+        console.log(diceRoll)
+        die.innerText = diceRoll
+        console.log(diceRoll)
+    // })
+})
+}
+
