e1191b33d470537623cf69003905dd9ad8a954dc
Shannon Maloy <ShannonMaloy@Shannons-MBP.lan>
2020-07-21 20:29:58 -0400

    Large and Small Straight scoring working

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function rollDice(){
             diceRollResult[i] = diceRoll
             }
     
-    console.log(diceRollResult, turnDiceSelection)
+    // console.log(diceRollResult, turnDiceSelection)
     })
 
     addMatches(diceRollResult)
@@ -70,36 +70,45 @@ function addMatches (arr){
     return scoreOptions
 }
 
+//************************************************** */
 //Function to evaluate all scoring options based on the current dice roll
 function scoreParse (obj){
     let score = Object.values(obj)
+    let straightCheck = 0
     //fullHouseCheck is used for alternative method currently commented out
     // let fullHouseCheck = 0
     
     //FOR EACH loop to check for 3, 4, and 5 of a kinds.  Did not use if/else if since 
     //a 5 of a kind can also be a 3 or 4 of a kind, a 4 of a kind can also be a 3 of a kind.
-    score.forEach((val,index) => {
+    score.forEach((number,index) => {
         //Check for 5 of a Kind AKA Yahtzee
-        if (val >= 5){
+        if (number >= 5){
             // console.log(index)
-            // console.log(val)
-            console.log('5 of a kind', ((index + 1) * val))
+            // console.log(number)
+            console.log('5 of a kind', ((index + 1) * number))
         } 
         //Check for 4 of a kind
-        if (val >= 4){
+        if (number >= 4){
             // console.log(index)
-            // console.log(val)
-            console.log('4 of a kind', ((index + 1) * val))
+            // console.log(number)
+            console.log('4 of a kind', ((index + 1) * number))
         } 
         //Check for 3 of a kind
-        if (val >= 3){
+        if (number >= 3){
             // console.log(index)
-            // console.log(val)
-            console.log('3 of a kind', ((index + 1) * val))
-        }  
+            // console.log(number)
+            console.log('3 of a kind', ((index + 1) * number))
+        }
     })
     //Two methods for solving Full House Check
-    // for (i = 0; i < score.length; i++){
+    let fullHouse = score.filter(function (element){
+        return (element > 1 && element < 4)
+    }) 
+    fullHouse = fullHouse.sort()
+    console.log(fullHouse,score)
+    if (fullHouse[0] === 2 && fullHouse[1] === 3){
+        console.log('full house!')}
+     // for (i = 0; i < score.length; i++){
     //     console.log("Inside Method 2", score[i])
         
     //     if (score[i] === 2 || score[i] === 3){
@@ -110,16 +119,33 @@ function scoreParse (obj){
     //         console.log("Full House Method 2")
     //     }
     // }
-    let fullHouse = score.filter(function (element){
-        return (element > 1 && element < 4)
-    }) 
-    fullHouse = fullHouse.sort()
-    console.log(fullHouse,score)
-    if (fullHouse[0] === 2 && fullHouse[1] === 3){
-        console.log('full house!')}
+    //Large and Small Straights Check
+    // console.log(score)
+    // let straightCheck = score.filter(function (element){
+    //     return (element === 1)
+    // })
+    // console.log("Straight Check: ", straightCheck.length)
+    // if (straightCheck.length === 4){
+    //     console.log('Small straight')
+    // } else if (straightCheck.length === 5){
+    //     console.log("Large Straight")
+    // }
+         // console.log('Score Index: ', score[index],' Score Index + 1: ',score[index+1])
+    for (let i = 0; i < score.length - 1; i++){
+        if ( score[i+1] >= 1 && score[i] >= 1){
+            straightCheck += 1
+        } 
+    }
+    if(straightCheck === 3){
+        console.log("Small straight")
+    } else if (straightCheck === 4){
+        console.log("Large Straight")
     }
 
-// console.log("help")
+
+
+}
+///End Score Parse Function
 
 // //Straight Check
 // let diceRollResultSorted = diceRollResult.sort()
